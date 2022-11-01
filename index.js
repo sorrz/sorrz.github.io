6cbb38150f1afcdbfc17d9a9ec9ec2cd94e8ef61
Dan Stjärnborg <dan.stjarnborg@gmail.com>
2022-11-01 10:03:12 +0100

    Update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const menu = document.getElementById("menu");
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
 
-Array.from(document.getElementsByClassName("menu-item"))
-    .forEach((item, index) => {
-        item.onmouseover = () => {
-            menu.dataset.activeIndex = index;
-        }
-    });
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
